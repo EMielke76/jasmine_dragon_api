1cbb151dc251f96926f6a9bb1c8cdb6333873c04
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-04-12 12:50:36 -0600

    Subscriptions validating tea_name

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Subscription do
 
   describe 'validations' do
     it { should validate_presence_of(:tea_id) }
+    it { should validate_presence_of(:tea_name) }
     it { should validate_numericality_of(:price) }
-    it { should define_enum_for(:status).with([:active, :inactive]) }
-    it { should define_enum_for(:frequency).with([:weekly, :bi_weekly, :monthly]) }
+    it { should define_enum_for(:status).with_values([:active, :inactive]) }
+    it { should define_enum_for(:frequency).with_values([:weekly, :bi_weekly, :monthly]) }
   end
 end
