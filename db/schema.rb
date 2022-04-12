c96dbda415ded3746bc88539e63ddbbda12c7146
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-04-12 09:24:07 -0600

    Add tea_name to subscriptions table:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_205302) do
+ActiveRecord::Schema.define(version: 2022_04_12_152305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_04_11_205302) do
     t.integer "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tea_name"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
