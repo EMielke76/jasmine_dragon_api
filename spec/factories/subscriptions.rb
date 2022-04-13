46dfa82ba575cebc11dd2e9f018cb4a5d290f4c6
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-04-13 12:56:04 -0600

    Set price in factory to nil

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :subscription do
     tea_id { Faker::Invoice.creditor_reference }
     tea_name { Faker::Color.color_name }
-    price { Faker::Number.number(digits: 4) }
+    price { nil }
     status { 0 }
     frequency { 0 }
     user
