0f27259b552b7212cd989ffe25639f72bb435f30
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-04-14 10:19:48 -0600

    Add module to rescue record not found

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -20,12 +20,14 @@ class Api::V1::SubscriptionsController < ApplicationController
     else
       render json: { status: 400, message: "#{sub.errors.full_messages.to_sentence}", data:{} }, status: :bad_request
     end
+    rescue ActiveRecord::RecordNotFound
+      save_and_continue
   end
 
   def destroy
     Subscription.find(params[:id]).destroy
     rescue ActiveRecord::RecordNotFound
-      render json: { status: 404, message: "Subscription Not Found", data:{} }, status: :not_found 
+      save_and_continue
   end
 
   private
--- spec/requests/api/v1/subscriptions/update_spec.rb
+++ spec/requests/api/v1/subscriptions/update_spec.rb
@@ -66,4 +66,33 @@ RSpec.describe "Update a subscription" do
       expect(Subscription.find(sub_2.id).price).to eq(900)
     end
   end
+
+  context 'sad path' do
+    it 'returns errors if sub doesnt exist' do
+      VCR.use_cassette('user-does-not-exist') do
+        user_1 = create(:user)
+
+        sub_1 = create(:subscription, user: user_1)
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+          params = ({status: "inactive"})
+
+        patch "/api/v1/users/#{user_1.id}/subscriptions/500", headers: headers, params: JSON.generate(params)
+
+        results = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(404)
+        expect(results).to be_a(Hash)
+
+        expect(results).to have_key(:status)
+        expect(results[:status]).to eq(404)
+
+        expect(results).to have_key(:message)
+        expect(results[:message]).to eq("Subscription Not Found")
+
+        expect(results).to have_key(:data)
+        expect(results[:data]).to eq({})
+      end
+    end
+  end
 end
